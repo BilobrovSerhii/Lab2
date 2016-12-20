ab880aa95bb871e446bcccbe0d6eb50241b47403
BilobrovSerhii <henecy12345>
2016-12-20 22:32:28 +0300

    LabVogon

---
+++ poezd.java
@@ -0,0 +1,21 @@
+public class poezd {
+
+ public static void main(String[] args) {
+   
+   vagon2 v1 = new vagon2();
+   v1.PustVagon(10);
+   v1.ZanyatxMesto(5);
+   v1.ZanyatxMesto(15);
+   v1.VagonInformac();
+
+   vagon2 v2 = new vagon2();
+   v2.IzmenitxChisloMest(16);
+   v2.PustVagon(14);
+   v2.ZanyatxMesto(5);
+   v2.ZanyatxMesto(7);
+   v2.ZanyatxMesto(5);
+   v2.VagonInformac();
+
+ }
+  
+}
